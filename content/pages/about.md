d0c184a8c44c539cb246f6786e59e5d72be68618
Agnieszka <agnieszka.kramarek@imapp.pl>
2016-09-07 14:47:58 +0200

    importing a custom css stylesheet

---
+++ content/pages/about.md
@@ -0,0 +1,10 @@
+Title: About 
+Date: 2016-09-07 08:00
+
+## Who I am
+
+Hi, I'm Agnieszka Kramarek and I live in Warsaw, Poland.
+
+## What I do
+
+I am a programmer currently working at [imapp](https://imapp.pl), building an awesome blockchain financial system on top of [Ethereum](https://ethereum.org).
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,6 +6,8 @@ AUTHOR = u'Agnieszka Kramarek'
 SITENAME = u'4gn3s coding'
 SITEURL = ''
 
+ROBOTS = 'index, follow'
+
 PATH = 'content'
 
 TIMEZONE = 'Europe/Warsaw'
@@ -20,10 +22,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+MAIN_MENU = True
+     
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -34,12 +42,28 @@ DEFAULT_PAGINATION = 5
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+STATIC_PATHS = ['images', 'extra']
+
+
+
 # Theme
 THEME = "/home/agnieszka/Sources/pelican-themes/Flex"
+
+BROWSER_COLOR = "#333"
+
+SOCIAL = (('facebook', 'https://www.facebook.com/agnieszka.kramarek'),
+          ('github', 'https://github.com/4gn3s'),
+          ('linkedin', ''),)
+          
+PYGMENTS_STYLE = 'monokai'
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/custom.css'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
+CUSTOM_CSS = 'static/custom.css'
 
 # Plugins
 PLUGINS = [
 ]
-
-# Static
-STATIC_PATHS = ['images', ]
